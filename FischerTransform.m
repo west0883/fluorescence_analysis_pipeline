58fa1846c2d3503a8d8bbb0b986dea01458b457e
Sarah West <west0883@umn.edu>
2022-05-26 09:18:31 -0500

    Function to perform Fisher transformation on Pearson correlation coefficients called from RunAnalysis.

---
+++ FischerTransform.m
@@ -0,0 +1,19 @@
+% FisherTransform.m
+% Sarah West
+% 5/9/22
+
+% Called by RunAnalysis, Applies the FisherTransform (arctanh(x)) to 
+% Pearson correlation coefficients.
+
+function parameters = FischerTransform(parameters)
+    
+    % Tell user what's happening.
+    MessageToUser('Transforming', parameters);
+   
+    % Apply transformation. Use cellfun if it's a cell. 
+    if iscell(parameters)
+        parameters.data_transformed = cellfun(@atanh, parameters.data, 'UniformOutput', false);
+    else
+         parameters.data_transformed = atanh(parameters.data);
+    end
+end 
