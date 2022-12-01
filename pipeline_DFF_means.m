fc59e5ade8b71083a1e1cc4768bbc1190723ba1c
Sarah West <west0883@umn.edu>
2022-12-01 12:00:18 -0600

    spaces

--- pipeline_DFF_means.m
+++ pipeline_DFF_means.m
@@ -9,7 +9,7 @@
 clear all; 
 
 % Create the experiment name.
-parameters.experiment_name='Random Motorized Treadmill';
+parameters.experiment_name = 'Random Motorized Treadmill';
 
 % Input directory name base
 parameters.dir_dataset_base = ['Y:\Sarah\Data\' parameters.experiment_name, '\'];
@@ -48,12 +48,12 @@ parameters.loop_variables.conditions_stack_locations = {'stacks'; 'spontaneous'}
 % Preprocessing parameters.
 % Sampling frequency of collected data (per channel), in Hz or frames per
 % second.
-parameters.sampling_freq=20; 
+parameters.sampling_freq = 20; 
 
 % Number of channels data was collected with. (2=had a blue and violet
 % channel, 1= had only blue and need to find blood vessel masks for
 % hemodynamic correction. 
-parameters.channelNumber=2;
+parameters.channelNumber = 2;
 
 % Number of pixels in the recorded image. Used to check that the 472 rig
 % did, indeed, record at the correct number of pixels (sometimes records at
@@ -74,7 +74,7 @@ parameters.blue_brighter = true;
 % calculates everything as DF/F.
 % 'vessel regression'-- regresses (blue) pixels against masks drawn from
 % blood vessels in the same (blue) channel. 
-parameters.correction_method ='regression';
+parameters.correction_method = 'regression';
 
 % Number of initial frames to skip, allows for brightness/image
 % stabilization of camera
@@ -89,63 +89,63 @@ parameters.pixel_cols = [50:100 150:200];
 
     % The nth stack in the collected data that you want to use for the
     % representative image
-    parameters.rep_stacki=1; 
+    parameters.rep_stacki = 1; 
 
     % The nth frame in the chosen stack that you want to use for the
     % representative image (after the skipped frames). 
-    parameters.rep_framei=1;
+    parameters.rep_framei = 1;
 
 % Across-day registration parameters
 
     % Set up transformation type (rigid, similar, or affine)
-    parameters.transformation='affine';
+    parameters.transformation = 'affine';
 
     % Determine configuration for intensity-based image registration using
     % imregconfig...(keep monomodal because images are similar intensity/contrast)
-    parameters.configuration='monomodal';
+    parameters.configuration = 'monomodal';
 
     % Set optimizer maximum step-length and iterations for performing registration
-    parameters.max_step_length=3e-3;
-    parameters.max_iterations=500;
+    parameters.max_step_length = 3e-3;
+    parameters.max_iterations = 500;
 
 % Do you want to mask your data? Yes--> mask_flag=true, No-->
 % mask_flag=false.
-parameters.mask_flag=true; 
+parameters.mask_flag = true; 
 
 % Do you want to temporally filter your data? Yes--> filter_flag=true, No-->
 % filter_flag=false.
-parameters.filter_flag=true; 
+parameters.filter_flag = true; 
 
 % Temporal filtering parameters. (These aren't used if filter_flag=false,
 % because no filtering is performed.) 
     % Order of Butterworth filter you want to use.
-    parameters.order=5; 
+    parameters.order = 5; 
 
     % Low cut off frequency
     %fc1=0.01; 
     
     % High cutoff frequency
-    parameters.fc2=7; 
+    parameters.fc2 = 7; 
 
     % Find Niquist freq for filter; sampling divided by 2
-    parameters.fn=parameters.sampling_freq/2; 
+    parameters.fn = parameters.sampling_freq/2; 
 
     % Find parameters of Butterworth filter. 
-    [parameters.b, parameters.a]=butter(parameters.order, parameters.fc2/parameters.fn,'low');
+    [parameters.b, parameters.a] = butter(parameters.order, parameters.fc2/parameters.fn,'low');
 
 % Set a minimum number of frames each channel of the stack needs to have to
 % consider it a valid stack for full processing. (If less than this, code 
 % will assume something very bad happened and won't continue processing the
 % stack, will jump to the next stack.) 
-parameters.minimum_frames=5980; 
+parameters.minimum_frames = 5980; 
 
 % Give list of individual frames to save from intermediate steps of
 % preprocessing from each stack to use for spot checking.
-parameters.frames_for_spotchecking=[1 500 1200 2400 3000]; 
+parameters.frames_for_spotchecking = [1 500 1200 2400 3000]; 
 
 % Set "upsampling factor" for dftregistration function (for within stack & 
 % within day registration); determines the sub-pixel resolution of the registration; 
-parameters.usfac=10;   
+parameters.usfac = 10;   
 
 
 % Re-run processing of random stack subset?
@@ -155,6 +155,7 @@ rerun = true;
 
 % Only run once
 if ~isfile([parameters.dir_exper 'preprocessing\stack means\mice_all_random.mat'])
+    
     create_RandomSubset_mice_all_RunAnalysis
 
     load('Y:\Sarah\Analysis\Experiments\Random Motorized Treadmill\preprocessing\stack means\mice_all_random.mat');
